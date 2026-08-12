c345a34c2a8d671fdee295c96ed7f54ac2c2812f
zhangxugang <zhangxugang@ccc.net>
2026-08-12 16:18:47 +0800

    chore: remove accidental commit helper files

--- _do_commit.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d d:\python\cherrystudio-for-houdini\web
-git add -A
-git commit -F COMMIT_MSG.txt
-set EXITCODE=%ERRORLEVEL%
-if exist COMMIT_MSG.txt del COMMIT_MSG.txt
-exit /b %EXITCODE%
